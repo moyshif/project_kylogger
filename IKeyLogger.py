55cc6fd38e1f570282dcb61dc6cf631e866f41f4
Moyshi <M0548452465@GMAIL.COM>
2025-02-19 14:16:34 +0200

    add file abstract

---
+++ IKeyLogger.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+class IKeyLogger(ABC):
+    """
+    ממשק מופשט עבור לוגר מקלדת.
+    מגדיר את הפעולות הנדרשות לכל יישום של לוגר מקלדת.
+    """
+    @abstractmethod
+    def start_logging(self) -> None:
+        """מתחיל את תהליך הרישום של לחיצות מקלדת."""
+        pass
+
+    @abstractmethod
+    def stop_logging(self) -> None:
+        """מפסיק את תהליך הרישום של לחיצות מקלדת."""
+        pass
+
+    @abstractmethod
+    def get_logged_keys(self) -> List[Dict[str, Dict[str, str]]]:
+        """
+        מחזיר את רשימת הלחיצות שנרשמו.
+
+        Returns:
+            List[Dict[str, Dict[str, str]]]: רשימה של מילונים, כאשר כל מילון מייצג רישום
+                                             עבור חלון פעיל, הכולל את הטקסט שנרשם ואת שפת המקלדת.
+        """
+        pass
+
